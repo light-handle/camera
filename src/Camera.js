afdeb517a6242eb2459b0f2b88831ef21d42c149
Kunal Thorat <774134+light-handle@users.noreply.github.com>
2023-02-10 23:36:38 -0500

    Front and back camera

--- src/Camera.js
+++ src/Camera.js
@@ -4,22 +4,29 @@ import Webcam from 'react-webcam';
 const Camera = () => {
   const webcamRef = useRef(null);
   const [imageSrc, setImageSrc] = useState(null);
+  const [facingMode, setFacingMode] = useState('user');
  
   const capture = () => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImageSrc(imageSrc);
+  };
+
+  const toggleCamera = () => {
+    setFacingMode(facingMode === 'user' ? 'environment' : 'user');
   };
  
   return (
     <div>
       <Webcam
         audio={false}
+        facingMode={facingMode}
         height={350}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
         width={350}
       />
       <button onClick={capture}>Capture photo</button>
+      <button onClick={toggleCamera}>Toggle camera</button>
       {imageSrc && <img src={imageSrc} alt="captured" />}
     </div>
   );
